2944dba216bfb584158aad5fba00b466c44c5e81
Tasmia-5 <128504724+Tasmia-5@users.noreply.github.com>
2023-03-22 20:32:31 -0400

    Updated

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("What is a number?")
+def factorial(n):
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(1, n+1):
+        result = result * i
+    else:
+        return n * factorial(n-1)
+
+if __name__ == '__main__':
+    num = 7
+    result = factorial(num)
+    print(f"The factorial of {num} is {result}.")
+    print(f"{result})
